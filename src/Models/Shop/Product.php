ba1973477b2562bf2ca7b4d0f9e543308699c9bd
Yuri Bondar <trigur@yandex.ru>
2018-07-10 11:30:54 +0300

    product model changes

--- src/Models/Shop/Product.php
+++ src/Models/Shop/Product.php
@@ -19,6 +19,52 @@ abstract class Product extends BaseModelI18n
     {
         return "/goods/{$this->id}";
     }
+
+    public function scopeWhereStyle($query, $styleId)
+    {
+        $styleRelationsTableName = config('tables.StyleProducts');
+        $productTableName = config('tables.Products');
+
+        return $query->join($styleRelationsTableName, function($join) use($styleRelationsTableName, $productTableName, $styleId) {
+            $join->on("{$styleRelationsTableName}.product_id", '=', "{$productTableName}.id")
+                ->where("{$styleRelationsTableName}.style_id", $styleId);
+        });
+    }
+
+    public function scopeWhereRoom($query, $roomId)
+    {
+        $roomRelationsTableName = config('tables.RoomProducts');
+        $productTableName = config('tables.Products');
+
+        return $query->join($roomRelationsTableName, function($join) use($roomRelationsTableName, $productTableName, $roomId) {
+            $join->on("{$roomRelationsTableName}.product_id", '=', "{$productTableName}.id")
+                ->where("{$roomRelationsTableName}.room_id", $roomId);
+        });
+    }
+
+    public function scopeWhereCategory($query, $categoryId)
+    {
+        $categoryRelationsTableName = config('tables.CategoryProducts');
+        $productTableName = config('tables.Products');
+
+        return $query->join($categoryRelationsTableName, function($join) use($categoryRelationsTableName, $productTableName, $categoryId) {
+            $join->on("{$categoryRelationsTableName}.product_id", '=', "{$productTableName}.id")
+                ->where("{$categoryRelationsTableName}.category_id", $categoryId);
+        });
+    }
+
+    public static function enabled()
+    {
+        $supplierTableName = config('tables.Suppliers');
+        $productTableName = config('tables.Products');
+
+        return self::select(\DB::raw("{$productTableName}.*"))
+            ->where("{$productTableName}.enabled", 1)
+            ->leftJoin("{$supplierTableName}", function($join) use($supplierTableName, $productTableName) {
+                $join->on("{$supplierTableName}.id", '=', "{$productTableName}.supplier_id")
+                    ->where("{$supplierTableName}.enabled", true);
+            });
+    }
 }
 
 
