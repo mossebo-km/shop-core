56d8cf38e7a52be741352c5cc2a8459f7351dbc6
Yuri Bondar <trigur@yandex.ru>
2018-07-29 15:30:27 +0300

    fix

--- database/migrations/2018_02_27_900920_create_order_products_table.php
+++ database/migrations/2018_02_27_900920_create_order_products_table.php
@@ -12,23 +12,24 @@ class CreateOrderProductsTable extends Migration
 
         Schema::create(config('tables.OrderProducts'), function (Blueprint $table) {
             $table->engine = "InnoDB";
+
+            $table->increments('id')->index();
+
             $table->integer('order_id')->unsigned()->index();
             $table->foreign('order_id')->references('id')->on(config('tables.Orders'))->onDelete('cascade');
 
             $table->integer('product_id')->unsigned()->index();
             $table->foreign('product_id')->references('id')->on(config('tables.Products'));
 
-            $table->text('product_params');
+            $table->integer('default_price')->unsigned();
+            $table->integer('final_price')->unsigned();
 
-            $table->string('title');
-            $table->text('description');
-            $table->integer('price')->unsigned()->index();
             $table->char('currency_code', 3);
             $table->foreign('currency_code')->references('code')->on(config('tables.Currencies'));
-            $table->integer('quantity')->unsigned();
-            $table->string('image_path')->nullable();
 
-            $table->primary(['order_id', 'product_id']);
+            $table->integer('quantity')->unsigned();
+
+            $table->timestamps();
         });
     }
 }
