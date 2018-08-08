0d727738359d3b5dd942617b3c502729aa027b28
Yuri Bondar <trigur@yandex.ru>
2018-08-08 12:37:53 +0300

    fix

--- src/Models/Region.php
+++ src/Models/Region.php
@@ -3,8 +3,11 @@
 namespace MosseboShopCore\Models;
 
 use MosseboShopCore\Models\Base\BaseModel;
+use MosseboShopCore\Support\Traits\Models\NestedTrait;
 
 abstract class Region extends BaseModel
 {
+    use NestedTrait;
+
     protected $tableIdentif = 'Regions';
 }
