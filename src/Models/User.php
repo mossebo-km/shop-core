a16d68c66b3e05d4f7f1c75c25cb0086f966949a
Yuri Bondar <trigur@yandex.ru>
2018-08-15 17:53:41 +0300

    fix

--- src/Models/User.php
+++ src/Models/User.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace MosseboShopCore\Models;
+namespace MosseboShopCore\Contracts\Models;
 
 use MosseboShopCore\Models\Base\Authenticatable;
 
 abstract class User extends Authenticatable
 {
-
+    protected $relationFieldName = 'user_id';
 }
