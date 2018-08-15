bd9d09a1ee5437f7f52608968bfe215dd34653a4
Yuri Bondar <trigur@yandex.ru>
2018-08-15 17:55:59 +0300

    fix

--- src/Contracts/Models/User.php
+++ src/Contracts/Models/User.php
@@ -5,5 +5,5 @@ namespace MosseboShopCore\Contracts\Models;
 interface User
 {
     public function ordersCount(): integer;
-    public function getPromoCodeUsesNum(integer $promoCodeId): integer;
+    public function getPromoCodeUsesNum($promoCodeId): integer;
 }
