834888e2dec8d4513cd2d2c17ab2bd62992a6a92
Yuri Bondar <trigur@yandex.ru>
2018-08-23 12:46:29 +0300

    fix

--- src/Models/Shop/Promo/PromoCode.php
+++ src/Models/Shop/Promo/PromoCode.php
@@ -2,11 +2,11 @@
 
 namespace MosseboShopCore\Models\Shop\Promo;
 
-use MosseboShopCore\Models\Base\BaseModel;
+use MosseboShopCore\Models\Base\BaseModelI18n;
 use MosseboShopCore\Contracts\Shop\Order\Order;
 use MosseboShopCore\Contracts\Shop\Order\OrderProduct;
 
-abstract class PromoCode extends BaseModel
+abstract class PromoCode extends BaseModelI18n
 {
     protected $tableIdentif = 'PromoCodes';
     protected $relationFieldName = 'promo_code_id';
---
+++ src/Models/Shop/Promo/PromoCodeI18n.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MosseboShopCore\Models\Shop;
+
+use MosseboShopCore\Models\Base\BaseModel;
+
+abstract class PromoCodeI18n extends BaseModel
+{
+    protected $tableIdentif = 'PromoCodesI18n';
+}
