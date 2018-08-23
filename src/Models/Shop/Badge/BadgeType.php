761ba8c20027d3efdf215d308230c43de246e150
Yuri Bondar <trigur@yandex.ru>
2018-08-23 17:33:32 +0300

    fix

--- src/Models/Shop/Badge/BadgeType.php
+++ src/Models/Shop/Badge/BadgeType.php
@@ -7,4 +7,5 @@ use MosseboShopCore\Models\Base\BaseModelI18n;
 abstract class BadgeType extends BaseModelI18n
 {
     protected $tableIdentif = 'BadgeTypes';
+    protected $relationFieldName = 'badge_type_id';
 }
