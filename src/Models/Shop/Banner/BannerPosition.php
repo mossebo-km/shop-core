2d27c26e8e2c30048dc727f50cd3a8637c5f77eb
Yuri Bondar <trigur@yandex.ru>
2018-08-31 15:43:50 +0300

    fix

--- src/Models/Shop/Banner/BannerPosition.php
+++ src/Models/Shop/Banner/BannerPosition.php
@@ -7,4 +7,5 @@ use MosseboShopCore\Models\Base\BaseModel;
 abstract class BannerPosition extends BaseModel
 {
     protected $tableIdentif = 'BannerPositions';
+    protected $relationFieldName = 'banner_position_id';
 }
---
+++ src/Models/Shop/Banner/BannerPositionRelation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MosseboShopCore\Models\Shop\Banner;
+
+use MosseboShopCore\Models\Base\BaseModel;
+
+abstract class BannerPositionRelation extends BaseModel
+{
+    protected $tableIdentif = 'BannerPositionRelations';
+}
