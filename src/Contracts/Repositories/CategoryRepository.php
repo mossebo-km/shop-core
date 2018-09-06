72265740bc70c63270b2e39b9152688069b656ef
Yuri Bondar <trigur@yandex.ru>
2018-09-06 12:26:00 +0300

    fix

--- src/Contracts/Repositories/CategoryRepository.php
+++ src/Contracts/Repositories/CategoryRepository.php
@@ -4,5 +4,5 @@ namespace MosseboShopCore\Contracts\Repositories;
 
 interface CategoryRepository extends Repository
 {
-    public function getTree($modificators);
+    public function getTree();
 }
