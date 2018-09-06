356975ea30b021f6389808aa8231b3cc1dbf6354
Yuri Bondar <trigur@yandex.ru>
2018-09-06 12:40:36 +0300

    fix

---
+++ src/Repositories/Storage/Cache.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MosseboShopCore\Repositories\Storage;
+
+use Illuminate\Cache\Repository;
+use MosseboShopCore\Contracts\Repositories\Storage as StorageInterface;
+
+class Cache extends Repository implements StorageInterface {
+
+}
