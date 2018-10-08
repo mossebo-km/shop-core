9f913aec02460b0a51d1e3df476fa36d3e8c93e4
Yuri Bondar <trigur@yandex.ru>
2018-10-08 11:52:57 +0300

    fix migrations

--- database/migrations/2018_07_24_143890_create_banner_place_relations_table.php
+++ database/migrations/2018_07_24_143890_create_banner_place_relations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBannerPositionRelationsTable extends Migration
+class CreateBannerPlaceRelationsTable extends Migration
 {
     public function up()
     {
